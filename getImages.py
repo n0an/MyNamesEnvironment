220b5988555a52d1acfe578c16eb674cbed06ec8
v01d <nantog@gmail.com>
2017-04-05 13:23:34 +0300

    getImages.py: Refactoring done Images gathering from ENG pages implemented

--- getImages.py
+++ getImages.py
@@ -7,118 +7,98 @@ import os
 import shutil
 import openpyxl
 
+# Helper methods
+# ** Checking found CSS element
+def is_element_width_ok(element):
+    element_width = int(element.get_attribute('width'))
+    return element_width > 190
+
+def is_element_src_ok(element):
+    element_scr_contains_lotr_traits = element.get_attribute('src').find("wikia.nocookie.net/lotr/images") != -1
+    return element_scr_contains_lotr_traits
+
+def is_element_extension_ok(element):
+    element_src = element.get_attribute('src')
+    return element_src.find('.jpg') != -1 or element_src.find('.png') != 1
+
+def is_element_ok(element):
+    is_width_ok     = is_element_width_ok(element)
+    is_src_ok       = is_element_src_ok(element)
+    is_extension_ok = is_element_extension_ok(element)
+    return is_width_ok and is_src_ok and is_extension_ok
+
+def save_image_for_url(url, imagesCounter = -1):
+    urlEndPosition = url.find('/revision')
+    cleanedImgSrc = url[:urlEndPosition]
+    print('Link found: ' + cleanedImgSrc)
+    driver.get(cleanedImgSrc)
+
+    image_file_name = imagename
+
+    if imagesCounter == -1:
+        image_file_name = imagename + '.jpg'
+    else:
+         image_file_name = imagename + str(imagesCounter) + '.jpg'
+
+    driver.get_screenshot_as_file(image_file_name)
+    print('***** Image saved!!! *****')
+
+def save_images_for_urlsList(urls_list):
+    imagesCounter = 1
+    for url in urls_list:
+        save_image_for_url(url, imagesCounter)
+        imagesCounter += 1
+
+def save_first_image_for_urlsList(urls_list):
+    save_image_for_url(urls_list[0])
+
+
+# +++++++++++++++ SCRIPT START +++++++++++++++
 # Windows ver:
-# driver = webdriver.Chrome("C:/chromedriver.exe")
+driver = webdriver.Chrome("C:/chromedriver.exe")
 
 # MacOS ver:
-driver = webdriver.Chrome("../chromedriver")
+# driver = webdriver.Chrome("../chromedriver")
 
 # === 1. Opening source workbook with names ===
-
 workbook = openpyxl.load_workbook('sourcetable.xlsx')
 sheet = workbook.get_sheet_by_name('sheet1')
 
-
-
-for index in range(2,100):
+# === 2. Iterating throught cells
+for index in range(7,15):
     name_cell       = sheet['B'+str(index)]
-    imagename_cell  = sheet['F'+str(index)]
     link_cell       = sheet['E'+str(index)]
-
     gender_cell     = sheet['C'+str(index)]
 
-    name = name_cell.value
-    imagename = 'FictionTolkien' + gender_cell.value + name
-    url = link_cell.value
+    name        = name_cell.value
+    imagename   = 'FictionTolkien' + gender_cell.value + name
+    url         = link_cell.value
 
-    print(' ^^^^^^^^^^^^^^ Start processing name: ' + name)
-
+    print('\n ^^^^^^^^^^^^^^ Start processing name: ' + name)
     driver.get(url)
 
     thumbnails_list = driver.find_elements_by_css_selector('.image.image-thumbnail>img')
 
-    selectedUrls = []
+    selected_urls = []
 
+    # *** Checking thumbnails for size, url_src, extension
     for thumbnail in thumbnails_list:
-        src = thumbnail.get_attribute('src')
+        if is_element_ok(thumbnail):
+            src = thumbnail.get_attribute('src')
+            selected_urls.append(src)
 
-        # print('Working with thumbnail: \n' + src + '\n')
+    # *** Printing selected URLs for name
+    print('selectedUrls for name ' + name + ':')
+    for selected_url in selected_urls:
+        print(selected_url)
 
-        if (int(thumbnail.get_attribute('width')) > 150) and (src.find("wikia.nocookie.net/lotr/images") != -1):
-            print('--Size and url location passed')
-            if src.find('.jpg') != -1 or src.find('.png') != 1:
-                print('==== extension passed!')
-                selectedUrls.append(src)
-
-    print('selectedUrls for name ' + name + ': \n')
-
-    imagesCounter = 1
-
-    for selectedUrl in selectedUrls:
-
-
-        print(selectedUrl)
-
-        urlEndPosition = selectedUrl.find('/revision')
-        cleanedImgSrc = selectedUrl[:urlEndPosition]
-        print('Link found: ' + cleanedImgSrc)
-        driver.get(cleanedImgSrc)
-
-        # Windows ver:
-        # driver.get_screenshot_as_file('C:/Users/nag/Desktop/aragorn.png')
-
-        # MacOS ver:
-
-        image_file_name = imagename + str(imagesCounter) + '.jpg'
-        imagesCounter += 1
-        driver.get_screenshot_as_file(image_file_name)
-        print('***** Image saved!!! *****')
+    # *** Saving image for name
+    if len(selected_urls) > 0:
+        print('==== Saving process for name: ' + name + ':')
+        save_first_image_for_urlsList(selected_urls)
+    else:
+        print('No images found for name')
+        continue
 
 
 
-
-#
-#
-# # driver.get('http://lotr.wikia.com/wiki/Aragorn_II_Elessar')
-# driver.get('http://lotr.wikia.com/wiki/Melkor')
-#
-# # x = driver.find_element_by_xpath("//*[@id='mw-content-text']/div[3]/div[2]/div[1]/div/a/img")
-#
-# linksList = driver.find_elements_by_css_selector('.image.image-thumbnail>img')
-# # x = driver.find_element_by_css_selector('.image.image-thumbnail>img')
-#
-#
-# selectedLinks = []
-#
-# if len(linksList) > 1:
-#     print('More than 1 link found:')
-#     for link in linksList:
-#         src = link.get_attribute('src')
-#         print('Working with link: \n' + src + '\n')
-#         if (int(link.get_attribute('width')) > 200) and (src.find("wikia.nocookie.net/lotr/images") != -1):
-#             print('--Size and vignette passed')
-#             if src.find('.jpg') != -1 or src.find('.png') != 1:
-#                 print('==== extension passed!')
-#                 selectedLinks.append(link)
-#
-# else:
-#     print('Only 1 link found:')
-#     print(linksList[0].get_attribute('src'))
-#
-# print('Selected links:')
-#
-# for link in selectedLinks:
-#     imgSrc = link.get_attribute('src')
-#
-#     print(imgSrc)
-#
-#     linkEndPosition = imgSrc.find('/revision')
-#     cleanedImgSrc = imgSrc[:linkEndPosition]
-#     print('Link found: ' + cleanedImgSrc)
-#     driver.get(cleanedImgSrc)
-#
-#     # Windows ver:
-#     # driver.get_screenshot_as_file('C:/Users/nag/Desktop/aragorn.png')
-#
-#     # MacOS ver:
-#     driver.get_screenshot_as_file('./aragorn.png')
