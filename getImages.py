7f97e3989bedf29eb2b5537f3f2901a25d0ffa4e
v01d <nantog@gmail.com>
2017-04-05 17:16:40 +0300

    getImages.py - done: - downloading images realized using urllib.request.urlretrieve() - number of current name added to print()

--- getImages.py
+++ getImages.py
@@ -2,10 +2,10 @@
 # coding: utf-8
 
 from selenium import webdriver
-
 import os
 import shutil
 import openpyxl
+import urllib.request
 
 # Helper methods
 # ** Checking found CSS element
@@ -17,16 +17,28 @@ def is_element_src_ok(element):
     element_scr_contains_lotr_traits = element.get_attribute('src').find("wikia.nocookie.net/lotr/images") != -1
     return element_scr_contains_lotr_traits
 
+def is_element_contains_extension(str, extension):
+    return str.find(extension) != -1
+
+def get_image_extension(url):
+    if is_element_contains_extension(url, ".jpg"):
+        return '.jpg'
+    elif is_element_contains_extension(url, ".png"):
+        return '.png'
+    elif is_element_contains_extension(url, ".jpeg"):
+        return '.jpeg'
+    else:
+        return '.file'
+
 def is_element_extension_ok(element):
     element_src = element.get_attribute('src')
-    return element_src.find('.jpg') != -1 or element_src.find('.png') != 1
+    return is_element_contains_extension(element_src, '.jpg') or is_element_contains_extension(element_src, '.png') or is_element_contains_extension(element_src, '.jpeg')
 
 def is_element_ok(element):
     is_width_ok     = is_element_width_ok(element)
     is_src_ok       = is_element_src_ok(element)
     is_extension_ok = is_element_extension_ok(element)
     return is_width_ok and is_src_ok and is_extension_ok
-
 
 
 
@@ -49,8 +61,9 @@ def save_image_for_url(url, rusUrl = False, imagesCounter = -1):
     else:
          image_file_name = imagename + str(imagesCounter) + '.jpg'
 
-    driver.get_screenshot_as_file(image_file_name)
+    driver.get_screenshot_as_file(dir_path + '/' + image_file_name)
     print('***** Image saved!!! *****')
+
 
 
 def save_images_for_urlsList(urls_list):
@@ -66,6 +79,43 @@ def rus_save_first_image_for_urlsList(urls_list):
     save_image_for_url(urls_list[0], True)
 
 
+def retrieve_image_for_url(url, rusUrl = False, imagesCounter = -1):
+    cleanedImgSrc = url
+
+    if not rusUrl:
+        urlEndPosition = url.find('/revision')
+        cleanedImgSrc = url[:urlEndPosition]
+
+    print('Link found: ' + cleanedImgSrc)
+
+    image_file_name = imagename
+
+    if imagesCounter == -1:
+        image_file_name = imagename + get_image_extension(url)
+    else:
+         image_file_name = imagename + str(imagesCounter) + get_image_extension(url)
+
+    urllib.request.urlretrieve(cleanedImgSrc, dir_path + '/' + image_file_name)
+    print('***** Image saved!!! *****')
+
+def retrieve_images_for_urlsList(urls_list):
+    imagesCounter = 1
+    for url in urls_list:
+        retrieve_image_for_url(url, imagesCounter)
+        imagesCounter += 1
+
+def retrieve_first_image_for_urlsList(urls_list):
+    retrieve_image_for_url(urls_list[0])
+
+def rus_retrieve_first_image_for_urlsList(urls_list):
+    retrieve_image_for_url(urls_list[0], True)
+
+
+def create_dir_for_names_images(category_path, gender):
+    dir_path = category_path + '/' + gender
+    os.makedirs(dir_path)
+    return dir_path
+
 # +++++++++++++++ SCRIPT START +++++++++++++++
 # Windows ver:
 driver = webdriver.Chrome("C:/chromedriver.exe")
@@ -77,17 +127,21 @@ driver = webdriver.Chrome("C:/chromedriver.exe")
 workbook = openpyxl.load_workbook('sourcetable.xlsx')
 sheet = workbook.get_sheet_by_name('sheet1')
 
+dir_path = create_dir_for_names_images('Fiction/Tolkien', 'Masc')
+
+
 # === 2. Iterating throught cells
-for index in range(7,15):
+for index in range(1,345):
     name_cell       = sheet['B'+str(index)]
     link_cell       = sheet['E'+str(index)]
     gender_cell     = sheet['C'+str(index)]
+
 
     name        = name_cell.value
     imagename   = 'FictionTolkien' + gender_cell.value + name
     url         = link_cell.value
 
-    print('\n ^^^^^^^^^^^^^^ Start processing name: ' + name)
+    print('\n' + str(index) + '. ^^^^^^^^^^^^^^ Start processing name: ' + name)
     driver.get(url)
 
     thumbnails_list = driver.find_elements_by_css_selector('.image.image-thumbnail>img')
@@ -105,12 +159,12 @@ for index in range(7,15):
     for selected_url in selected_urls:
         print(selected_url)
 
-
     # *** Saving image for name
     # *** Checking if there're at least 1 url found using ENG URL.
     if len(selected_urls) > 0:
         print('==== Saving process for name: ' + name + ':')
-        save_first_image_for_urlsList(selected_urls)
+        # save_first_image_for_urlsList(selected_urls)
+        retrieve_first_image_for_urlsList(selected_urls)
     else:
         # *** No ENG URLs found. Try to find images using RUS URLs
         print('--- No images found for name using ENG URL')
@@ -143,7 +197,8 @@ for index in range(7,15):
 
         if len(selected_rusurls) > 0:
             print('==== Saving process for name: ' + name + ' (from RUS URL):')
-            rus_save_first_image_for_urlsList(selected_rusurls)
+            # rus_save_first_image_for_urlsList(selected_rusurls)
+            rus_retrieve_first_image_for_urlsList(selected_rusurls)
         else:
             print('--- No images found for name using RUS URL')
             print('++++++ Going to the next name ++++++')
