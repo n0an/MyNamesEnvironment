485e127483349feea71dc7d8907074cec682bf41
n0an <nantog@gmail.com>
2017-09-19 23:34:19 +0300

    getImagesFromSRCLinks.py fixed

--- 07. Collect images/getImagesFromSRCLinks.py
+++ 07. Collect images/getImagesFromSRCLinks.py
@@ -12,6 +12,19 @@ def create_dir_for_names_images(category_path):
     os.makedirs(dir_path)
     return dir_path
 
+def is_element_contains_extension(str, extension):
+    return str.find(extension) != -1
+
+def get_image_extension(url):
+    if is_element_contains_extension(url, ".jpg"):
+        return '.jpg'
+    elif is_element_contains_extension(url, ".png"):
+        return '.png'
+    elif is_element_contains_extension(url, ".jpeg"):
+        return '.jpeg'
+    else:
+        return '.file'
+
 def retr_image_for_url(url):
     cleanedImgSrc = url
 
@@ -21,19 +34,19 @@ def retr_image_for_url(url):
 
     image_file_name = imagename + get_image_extension(url)
 
-    urllib.request.urlretrieve(cleanedImgSrc, dir_path + '/' + image_file_name)
+    urllib.request.urlretrieve(cleanedImgSrc, dir_path + image_file_name)
     print('***** Image saved!!! *****')
 
 
 # +++++++++++++++ SCRIPT START +++++++++++++++
 
 # *** SCRIPT CONFIGURATION:
-cell_start_number   = 2
-cell_end_number     = 28
+cell_start_number   = 29
+cell_end_number     = 32
 dir_path = create_dir_for_names_images('Fiction/GOT/')
 
 # *** SCRIPT START:
-# os.chdir('!WORKFLOW')
+os.chdir('../!WORKFLOW')
 
 # === 1. Opening source workbook with names ===
 workbook = openpyxl.load_workbook('DoneTable.xlsx')
@@ -43,9 +56,9 @@ sheet = workbook.get_sheet_by_name('sheet1')
 for index in range(cell_start_number ,cell_end_number + 1):
     name_cell           = sheet['B'+str(index)]
     eng_url_cell        = sheet['E'+str(index)]
-    rus_url_cell        = sheet['L'+str(index)]
+    rus_url_cell        = sheet['K'+str(index)]
     eng_image_url_cell  = sheet['F'+str(index)]
-    rus_image_url_cell  = sheet['M'+str(index)]
+    rus_image_url_cell  = sheet['L'+str(index)]
     gender_cell         = sheet['C'+str(index)]
     imagename_cell      = sheet['G'+str(index)]
 
