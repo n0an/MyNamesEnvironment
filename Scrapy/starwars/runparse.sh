0d48a6e9d9fafe15ecab8cce5d662d0e63864ee2
n0an <nantog@gmail.com>
2019-03-05 14:17:31 +0300

    LOG_LEVEL=ERROR, typos fixed

--- Scrapy/starwars/runparse.sh
+++ Scrapy/starwars/runparse.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # starwars parser based on eng category page
 # For example:
-# scrapy crawl gotstarknames -a category="https://starwars.fandom.com/wiki/Category:Males" -o males.csv
-scrapy crawl gotstarknames -a category="$1" -o $2
+# scrapy crawl starwars_spider -a category="https://starwars.fandom.com/wiki/Category:Males" -o males.csv
+scrapy crawl starwars_spider -a category="$1" -o $2
